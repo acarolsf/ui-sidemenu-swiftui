0695d37224a5c471c33c5638fb38b39894d5fbd2
Ana Carolina <cscarol20@gmail.com>
2021-04-17 13:43:06 -0300

    Initial Commit

---
+++ sidemenu-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sidemenu-swiftui
+//
+//  Created by Ana Carolina on 17/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sidemenu-swiftui/sidemenu_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sidemenu_swiftuiApp.swift
+//  sidemenu-swiftui
+//
+//  Created by Ana Carolina on 17/04/21.
+//
+
+import SwiftUI
+
+@main
+struct sidemenu_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
